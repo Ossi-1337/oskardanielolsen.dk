d56cb80fad8fb213ae8bbef73add862405b8983f
Ossi <113044607+Ossi-1337@users.noreply.github.com>
2025-11-17 10:54:00 +0100

    update

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ setInterval(() => {
     }
     document.title = arr[index];
 }, 1000);
+
+let rotate_amount = 360 / 500;
+let current_angle = 0;
+
+setInterval(() => {
+    current_angle += rotate_amount;
+    if (current_angle >= 360) {
+        current_angle = 0;
+    }
+    document.querySelector('.rotating-text').style.transform = `rotate(${current_angle}deg)`;
+}, 1);
